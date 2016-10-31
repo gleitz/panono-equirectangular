12291ee38f5ee319b62a274e6c174a87a6367778
Benjamin Gleitzman <gleitz@mit.edu>
2016-10-31 01:24:32 -0500

    Specify correct verison of Blender

--- convert.py
+++ convert.py
@@ -6,6 +6,14 @@ import requests
 import sys
 
 
+def download(panono_id):
+    api_url = 'https://api3-dev.panono.com/panorama/{}'.format(panono_id)
+    api_json = requests.get(api_url).json()
+    base_url = api_json['data']['images']['cubemaps'][0]['base_url']
+    image_id = re.match(r"https://tiles.panono.com/5/(\w+)/", base_url).group(1)
+    filename = '{}.jpg'.format(panono_id)
+    os.system('wget https://tiles.panono.com/5/' + image_id + '/equirectangular_8192.jpg -O ' + filename)
+
 def convert(panono_id):
     directions = ['0', '1', '5', '4', '3', '2']
     filenames = []
@@ -42,4 +50,4 @@ if __name__ == '__main__':
         print 'usage: python convert.py panono_url1 [panono_url2 panono_url3...]'
     for panono_url in sys.argv[1:]:
         panono_id = re.match(r"https://www.panono.com/p/(\w+)", panono_url).group(1)
-        convert(panono_id)
+        download(panono_id)
